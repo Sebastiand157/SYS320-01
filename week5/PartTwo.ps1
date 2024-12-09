32f729e49fb0deb1e90177aa5641313b4261854c
Sebastiand157 <113308434+Sebastiand157@users.noreply.github.com>
2024-12-08 23:23:51 -0500

    Add files via upload

---
+++ week5/PartTwo.ps1
@@ -0,0 +1,7 @@
+﻿﻿. (Join-Path $PSScriptRoot Scrape.ps1)
+
+# List all the classes of JOYC 310 on Mondays, only display Class Code and Time
+# Sort by Start Time
+$FullTable | where-Object{ ($_.Location -ilike "JOYC 310") -and ($_.days -contains "Monday") } | `
+            sort-Object "Start Time" | `
+            select "Start Time", "End Time", "Class Code"
