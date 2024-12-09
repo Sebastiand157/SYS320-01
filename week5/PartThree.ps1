4ff189f2e387c437ac052dfc96c84b589adead3b
Sebastiand157 <113308434+Sebastiand157@users.noreply.github.com>
2024-12-08 23:38:14 -0500

    Add files via upload

---
+++ week5/PartThree.ps1
@@ -0,0 +1,23 @@
+﻿﻿. (Join-Path $PSScriptRoot .Scrape.ps1)
+
+# Make a list of all the instructors that teach at least 1 course in
+# SYS, SEC, NET, FOR, CSI, DAT
+# Sort by name, and make it unique
+
+$ITSInstructors = $FullTable | where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                              ($_."Class Code" -ilike "SEC*") -or `
+                                              ($_."Class Code" -ilike "NET*") -or `
+                                              ($_."Class Code" -ilike "FOR*") -or `
+                                              ($_."Class Code" -ilike "CSI*") -or `
+                                              ($_."Class Code" -ilike "DAT*") } `
+                             | sort-Object "Instructor" `
+                             | select "Instructor" -Unique
+$ITSInstructors
+
+
+
+
+
+
+
+
