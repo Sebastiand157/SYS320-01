be62465a49145fd2675937b358275d69aae342ca
Sebastiand157 <113308434+Sebastiand157@users.noreply.github.com>
2024-12-08 23:07:12 -0500

    Add files via upload

---
+++ week5/PartOne.ps1
@@ -0,0 +1,7 @@
+﻿. (Join-Path $PSScriptRoot Scrape.ps1)
+
+$FullTable = daysTranslator(gatherClasses)
+
+# List all the classes of Instructor Furkan Paligu
+$FullTable | Select "Class Code", "Title", Instructor, Location, Days, "Start Time", "End Time" | `
+            Where{ $_."Instructor" -ilike "Furkan Paligu" }
